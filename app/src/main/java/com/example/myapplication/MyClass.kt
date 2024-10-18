08af7ef35b85bdf2a20ba9da33f72a3717574e71
Sagnik-web <clicksagnikbiswas01@gmail.com>
2024-10-18 16:31:20 +0530

    For Each Loop

--- app/src/main/java/com/example/myapplication/MyClass.kt
+++ app/src/main/java/com/example/myapplication/MyClass.kt
@@ -11,16 +11,33 @@ class MyClass : NewClass() {
 //            a.Name()
 
 //            abc()
-            var a = 0
-            for (i in 0 until 10){
-             println(a)
-                a += 1
-            }
 
-            for (i in 10 downTo  0){
-                println(i)
+//            var a = 0
+//            for (i in 0 until 10){
+//             println(a)
+//                a += 1
+//            }
+//
+//            for (i in 10 downTo  0){
+//                println(i)
+//
+//            }
 
-            }
+
+//            var arr = ArrayList<Int>()
+//
+//            arr.add(10)
+//            arr.add(20)
+//            arr.add(30)
+//            arr.add(40)
+//            arr.add(50)
+//
+//            for(i in arr){
+//                println("Number: $i")
+//            }
+
+
+
         }
 
 
